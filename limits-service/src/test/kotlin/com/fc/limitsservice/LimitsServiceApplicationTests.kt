11a8c84adb9c26375259340d2abfb1c6b6b50f08
Casper Feng <casp.feng@gmail.com>
2020-12-29 15:44:38 +0100

    initalize limits-service

---
+++ limits-service/src/test/kotlin/com/fc/limitsservice/LimitsServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.fc.limitsservice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class LimitsServiceApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
