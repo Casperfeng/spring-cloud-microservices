aee84e932ff4a9a964c54bc1286e9a42ffb6b91b
Casper Feng <casp.feng@gmail.com>
2020-12-30 18:35:19 +0100

    bypass reserved SQL keywords

--- currency-exchange-service/src/main/kotlin/com/fc/currencyexchangeservice/domain/ExchangeValue.kt
+++ currency-exchange-service/src/main/kotlin/com/fc/currencyexchangeservice/domain/ExchangeValue.kt
@@ -1,6 +1,7 @@
 package com.fc.currencyexchangeservice.domain
 
 import java.math.BigDecimal
+import javax.persistence.Column
 import javax.persistence.Entity
 import javax.persistence.Id
 
@@ -8,7 +9,9 @@ import javax.persistence.Id
 data class ExchangeValue(
         @Id
         var id: Long = -1,
+        @Column(name="currency_from")
         var from: String = "",
+        @Column(name="currency_to")
         var to: String = "",
         var conversionMultiple: BigDecimal = BigDecimal.valueOf(-1),
         var port: Int = 8000) {}
