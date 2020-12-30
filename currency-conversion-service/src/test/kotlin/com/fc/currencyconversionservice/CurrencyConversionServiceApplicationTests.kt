0a41f5f546d0484d977f9f5c9391c2809870b236
Casper Feng <casp.feng@gmail.com>
2020-12-30 22:14:18 +0100

    initialize currency-conversion-service

---
+++ currency-conversion-service/src/test/kotlin/com/fc/currencyconversionservice/CurrencyConversionServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.fc.currencyconversionservice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CurrencyConversionServiceApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
