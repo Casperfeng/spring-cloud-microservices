85d0ed92a751901800f70dec5ee2ad637b973250
Casper Feng <casp.feng@gmail.com>
2021-01-02 17:28:22 +0100

    rename feign client

--- currency-conversion-service/src/main/kotlin/com/fc/currencyconversionservice/proxy/CurrencyExchangeServiceProxy.kt
+++ currency-conversion-service/src/main/kotlin/com/fc/currencyconversionservice/proxy/CurrencyExchangeServiceProxy.kt
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 
-@FeignClient(name="currency-conversion-service", url="localhost:8000")
+@FeignClient(name="currency-exchange-service", url="localhost:8000")
 interface CurrencyExchangeServiceProxy {
     @GetMapping("/currency-exchange/from/{from}/to/{to}")
     fun retrieveExchangeValue(@PathVariable from: String, @PathVariable to: String): CurrencyConversionBean
