4a77e57374c2936e84dce5adab03afaf54342bdd
Casper Feng <casp.feng@gmail.com>
2021-01-03 01:34:41 +0100

    downgrade spring boot and add ribbon load balancer

--- currency-conversion-service/src/main/kotlin/com/fc/currencyconversionservice/proxy/CurrencyExchangeServiceProxy.kt
+++ currency-conversion-service/src/main/kotlin/com/fc/currencyconversionservice/proxy/CurrencyExchangeServiceProxy.kt
@@ -1,11 +1,13 @@
 package com.fc.currencyconversionservice.proxy
 
 import com.fc.currencyconversionservice.domain.CurrencyConversionBean
+import org.springframework.cloud.netflix.ribbon.RibbonClient
 import org.springframework.cloud.openfeign.FeignClient
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 
-@FeignClient(name="currency-exchange-service", url="localhost:8000")
+@FeignClient(name="currency-exchange-service")
+@RibbonClient(name="currency-exchange-service")
 interface CurrencyExchangeServiceProxy {
     @GetMapping("/currency-exchange/from/{from}/to/{to}")
     fun retrieveExchangeValue(@PathVariable from: String, @PathVariable to: String): CurrencyConversionBean
