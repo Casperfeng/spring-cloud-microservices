b42acf937d0b9f54e63899075a85b9ebf9ade44b
Casper Feng <casp.feng@gmail.com>
2021-01-05 15:52:01 +0100

    connect currency service with eureka client

--- currency-conversion-service/src/main/kotlin/com/fc/currencyconversionservice/CurrencyConversionServiceApplication.kt
+++ currency-conversion-service/src/main/kotlin/com/fc/currencyconversionservice/CurrencyConversionServiceApplication.kt
@@ -2,10 +2,12 @@ package com.fc.currencyconversionservice
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient
 import org.springframework.cloud.openfeign.EnableFeignClients
 
 @SpringBootApplication
 @EnableFeignClients("com.fc.currencyconversionservice.proxy")
+@EnableDiscoveryClient
 class CurrencyConversionServiceApplication
 
 fun main(args: Array<String>) {
