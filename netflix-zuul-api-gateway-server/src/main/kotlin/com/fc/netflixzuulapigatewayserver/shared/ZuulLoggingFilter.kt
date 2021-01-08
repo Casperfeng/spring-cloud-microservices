91b9bb9dbd555785672027612395a8743879dd28
Casper Feng <casp.feng@gmail.com>
2021-01-08 23:34:30 +0100

    add logging to zuul api gateway

---
+++ netflix-zuul-api-gateway-server/src/main/kotlin/com/fc/netflixzuulapigatewayserver/shared/ZuulLoggingFilter.kt
@@ -0,0 +1,32 @@
+package com.fc.netflixzuulapigatewayserver.shared
+
+import com.netflix.zuul.ZuulFilter
+import com.netflix.zuul.context.RequestContext
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import org.springframework.stereotype.Component
+import javax.servlet.http.HttpServletRequest
+
+@Component
+class ZuulLoggingFilter(): ZuulFilter() {
+
+    val logger = LoggerFactory.getLogger("package.ClassName")
+
+    override fun shouldFilter(): Boolean {
+        return true
+    }
+
+    override fun run(): Any {
+        val request: HttpServletRequest = RequestContext.getCurrentContext().request
+        logger.info("request -> $request request uri -> ${request.requestURI}")
+        return 0
+    }
+
+    override fun filterType(): String {
+        return "pre"
+    }
+
+    override fun filterOrder(): Int {
+        TODO("Not yet implemented")
+    }
+}
