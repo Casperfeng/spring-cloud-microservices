f68f2011f73e60366e996629ddb12c94c54c22ab
Casper Feng <casp.feng@gmail.com>
2021-01-09 11:28:00 +0100

    move proxy call to zuul gateway

--- currency-conversion-service/src/main/kotlin/com/fc/currencyconversionservice/proxy/CurrencyExchangeServiceProxy.kt
+++ currency-conversion-service/src/main/kotlin/com/fc/currencyconversionservice/proxy/CurrencyExchangeServiceProxy.kt
@@ -6,9 +6,9 @@ import org.springframework.cloud.openfeign.FeignClient
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 
-@FeignClient(name="currency-exchange-service")
+@FeignClient(name="netflix-zuul-api-gateway-server")
 @RibbonClient(name="currency-exchange-service")
 interface CurrencyExchangeServiceProxy {
-    @GetMapping("/currency-exchange/from/{from}/to/{to}")
+    @GetMapping("currency-exchange-service/currency-exchange/from/{from}/to/{to}")
     fun retrieveExchangeValue(@PathVariable from: String, @PathVariable to: String): CurrencyConversionBean
 }
